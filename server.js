10b2de678843d77ec07c4ce282b60631fceaa914
gn <gn@star-trac.de>
2020-03-26 16:13:56 +0100

    tutorial first steps

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello Mr. API server! :)' });
+});
+
 const callback = () => {
   //console.log(`\tApp is running at http://localhost:${port} in ${env} mode`);
   console.log('\tPress CTRL-C to stop\n');
