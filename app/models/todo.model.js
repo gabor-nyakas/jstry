057bd412e8242e0ce69c573d7414d80c769cefc1
gn <gn@star-trac.de>
2020-03-31 17:13:08 +0200

    small fixes

--- app/models/todo.model.js
+++ app/models/todo.model.js
@@ -1,18 +1,22 @@
 module.exports = (sequelize, Sequelize) => {
-  const Todo = sequelize.define('todo', {
-    title: {
-      type: Sequelize.STRING
+  const Todo = sequelize.define(
+    'Todo',
+    {
+      title: {
+        type: Sequelize.STRING
+      },
+      todoText: {
+        type: Sequelize.TEXT
+      },
+      published: {
+        type: Sequelize.BOOLEAN
+      },
+      created: {
+        type: Sequelize.DATE
+      }
     },
-    todoText: {
-      type: Sequelize.TEXT
-    },
-    published: {
-      type: Sequelize.BOOLEAN
-    },
-    created: {
-      type: Sequelize.DATE
-    }
-  });
+    { timestamps: false, freezeTableName: true }
+  );
 
   return Todo;
 };
