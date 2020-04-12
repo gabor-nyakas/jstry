9813a079b87d5bb3e8a6b110330de4a9c413e6a1
Gabor Nyakas <gabbyka@Gabors-MacBook-Air.local>
2020-04-12 21:42:47 +0200

    create

--- app/components/todo/todoService.js
+++ app/components/todo/todoService.js
@@ -17,3 +17,32 @@ export function findAll(req, res) {
       });
     });
 }
+
+// Create and Save a new Tutorial
+export function create(req, res) {
+  // Validate request
+  if (!req.body.title) {
+    res.status(400).send({
+      message: 'Content can not be empty!'
+    });
+    return;
+  }
+
+  // Create a Tutorial
+  const todo = {
+    title: req.body.title,
+    todoText: req.body.todoText,
+    published: req.body.published ? req.body.published : false
+  };
+
+  // Save Tutorial in the database
+  Todo.create(todo)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || 'Some error occurred while creating the Tutorial.'
+      });
+    });
+}
